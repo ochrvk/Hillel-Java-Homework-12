d680de34a81848d447b42e7e90f9956b785037fa
Sasha <cherevko.alexandr.1997@gmail.com>
2020-03-17 16:18:54 +0200

    first commit

---
+++ src/hw12/Main.java
@@ -0,0 +1,19 @@
+package hw12;
+
+public class Main {
+    public static String reverse(String message) {
+        String[] strings = message.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strings.length - 1; i >= 0; i--) {
+            sb.append(strings[i] + " ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String message  = "man has dog";
+        System.out.println(message);
+        message = reverse(message);
+        System.out.println(message);
+    }
+}
